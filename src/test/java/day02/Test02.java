74415fe399c695f0f0239ff44c0fce00ad1db7e1
Babayiğit <babayigit@gmail.com>
2024-08-01 11:52:43 +0300

    JUNIT_2

---
+++ src/test/java/day02/Test02.java
@@ -0,0 +1,16 @@
+package day02;
+
+public class Test02 {
+    /*
+1-https://babayigit.net/test/ adresine gidin
+2-Login butonuna tıklayın
+3-Username kısmına “babayigit” yazdırın
+4-Password kısmına baba123 yazdırın
+5-Login butonuna tıklayın
+6-Başarılı giriş yapılamadığını doğrulayın
+7-Login Sayfasında kullanıcı adına "babayigit" Pasword kısmına “Mb3471&” yazdırın.
+8-Login butonuna tıklayın
+9-Başarılı giriş yapıldığını doğrulayın
+10-Sayfayı kapatın
+*/
+}
---
+++ src/test/java/day02/Test03.java
@@ -0,0 +1,14 @@
+package day02;
+
+public class Test03 {
+/*
+        BeforeClass ile driver'i oluşturun ve class icinde static yapin
+        Maximize edin ve 10 sn bekletin
+        her biri için https://www.babayigit.net/test adresine gidin
+        1-arama kutusuna "12 Angry Men” yazip, cikan sonuc sayisini yazdirin
+        2-arama kutusuna "Vizontele” yazip, cikan sonuc sayisini yazdirin
+        3-arama kutusuna "Saving Private Ryan” yazip, cikan sonuc sayisini yazdirin
+        AfterClass ile kapatin.
+     */
+
+}
